2640b2e2c3244b6aa7eac92667ada48f663a8da0
Alexander Pavlov <workboxpavlov@yandex.ru>
2022-03-31 17:42:00 +0300

    Initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,28 @@
+// const   humburger = document.querySelector('.humburger'),
+//         menu = document.querySelector('.menu'),
+//         closeElem = document.querySelector('.menu__close');
+
+// humburger.addEventListener('click', function () {
+//         menu.classList.add('active');
+//     });
+// closeElem.addEventListener('click', () => {
+//     menu.classList.remove('active');
+// });
+
+
+
+const   humburger = document.querySelector('.humburger'),
+        menu = document.querySelector('.menu'),
+        closeElem = document.querySelector('.menu__close'),
+        closeElem2 = document.querySelector('.menu__overlay');
+
+humburger.addEventListener('click', ()=>{
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', ()=>{
+    menu.classList.remove('active');
+});
+closeElem2.addEventListener('click', ()=>{
+    menu.classList.remove('active');
+});
